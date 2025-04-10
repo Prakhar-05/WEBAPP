027b082059a696f4bea4f8712ab4cf1387f393ee
Prakhar Singhania <71015511+Prakhar-05@users.noreply.github.com>
2025-04-10 22:00:47 +0530

    Update apps.py

--- user_app/apps.py
+++ user_app/apps.py
@@ -1,6 +1,16 @@
 from django.apps import AppConfig
 
-
 class UserAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'user_app'
+
+    def ready(self):
+        from django.db.utils import OperationalError
+        try:
+            from myproject.create_admin import create_default_admin
+            create_default_admin()
+        except OperationalError:
+            pass  # Database might not be ready yet
+        except Exception as e:
+            print("Error creating default admin:", e)
+
