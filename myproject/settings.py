7f3be04d927f5ac0da99768dc2cc1eb528c620e2
Prakhar Singhania <71015511+Prakhar-05@users.noreply.github.com>
2025-04-10 08:21:53 +0530

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -96,6 +96,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # ─────────────── CORS HEADERS ───────────────
 CORS_ALLOW_ALL_ORIGINS = True
